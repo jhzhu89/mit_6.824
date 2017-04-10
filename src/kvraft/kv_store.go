14366a07383eb82084df8842e42d615a9af2b5a5
Jiahao <jiahao.zhu89@outlook.com>
2017-04-10 18:49:10 +0800

    remove RWMutex for kv_store since it is not needed

--- src/kvraft/kv_store.go
+++ src/kvraft/kv_store.go
@@ -1,11 +1,6 @@
 package raftkv
 
-import (
-	"sync"
-)
-
 type kvstore struct {
-	sync.RWMutex
 	store map[string]string
 }
 
@@ -14,20 +9,14 @@ func newKvstore() *kvstore {
 }
 
 func (k *kvstore) put(key, value string) {
-	k.Lock()
-	defer k.Unlock()
 	k.store[key] = value
 }
 
 func (k *kvstore) append(key, value string) {
-	k.Lock()
-	defer k.Unlock()
 	old := k.store[key]
 	k.store[key] = old + value
 }
 
 func (k *kvstore) get(key string) string {
-	k.RLock()
-	defer k.RUnlock()
 	return k.store[key]
 }
